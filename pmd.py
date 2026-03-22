490e6e7d234974f28bc8e37875fa3e5a3bc8eb9a
patricklubach <plubach1994@gmail.com>
2026-03-22 01:26:12 +0100

    feat: Add table from str parser func

--- pmd.py
+++ pmd.py
@@ -1,10 +1,36 @@
 import re
+import pandas as pd
+
 
 class MarkdownItem:
     def __init__(self, title, level, content: str = ""):
         self.title = title
         self.level = level
         self.content_raw = content
+        self.tables = []
+
+    def add_table_from_str(self, text):
+        text = """
+        | Some Title | Some Description             | Some Number |
+        |------------|------------------------------|-------------|
+        | Dark Souls | This is a fun game           | 5           |
+        | Bloodborne | This one is even better      | 2           |
+        | Sekiro     | This one is also pretty good | 110101      |
+        """
+
+        pattern = r"\| ([\w\s]+) \| ([\w\s]+) \| ([\w\s]+) \|"
+
+        # Use the findall function to extract all rows that match the pattern
+        matches = re.findall(pattern, text)
+
+        # Extract the header and data rows
+        header = matches[0]
+        data = matches[1:]
+
+        # Create a pandas DataFrame using the extracted header and data rows
+        df = pd.DataFrame(data, columns=header)
+
+        self.tables.append(df)
 
 
 class MarkdownDocument:
